e2bb2660821ea942de48bf59075a7160b502c987
Ridox_Studio <66634814+codad5@users.noreply.github.com>
2023-06-25 17:20:22 +0100

    fix

--- app/api/check-username-availability/route.ts
+++ app/api/check-username-availability/route.ts
@@ -7,13 +7,13 @@ import connectDb from "@/utils/mongoose";
 
 export const GET = async (req: Request) => {
     try {
-        const url = new URL(req.url);
+        const url = new URL(req.toString());
         const params = new URLSearchParams(url.search);
         const username = params.get("username")?.trim()
         if(!username) throw new Error("No username given")
         if(username.length < 4) throw new Error("username too short")
         await connectDb()
-        const avability = await User.findOne({ username : username.toLowerCase() }) ? false : true
+        const avability = await User.findOne({ username : username?.toLowerCase() }) ? false : true
         return NextResponse.json<CustomResponse<checkUsernameAvailabilityResponseData>>({
             success: true,
             message: avability ? "username available" : "username not available",
