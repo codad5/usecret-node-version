cefba99ab1e967c9eb3171ebd8ee1e55cb506afe
Ridox_Studio <66634814+codad5@users.noreply.github.com>
2023-06-25 16:39:44 +0100

    allow multiple null value for email and phone

--- utils/Models/User.ts
+++ utils/Models/User.ts
@@ -9,9 +9,10 @@ import { UsersModel } from "@/utils/types/Models";
 const UserSchema = new mongoose.Schema<UsersModel>({
     username: { type: String, required: true, unique: true },
     password: { type: String, required: false },
-    email: { type: String, required: false, unique: true },
-    phone : { type : String, required: false},
-})
+    email: { type: String, required: false, unique: true, sparse: true },
+    phone: { type: String, required: false, unique: true, sparse: true },
+});
+
 
 export default mongoose.models?.User || mongoose.model('User', UserSchema)
 
