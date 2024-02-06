ff3df0014eedd6325318bd52169c27d5c3f7e4b1
Ridox_Studio <66634814+codad5@users.noreply.github.com>
2024-02-06 08:18:04 +0100

    Add send_message function to send WhatsApp message

--- app/api/send-message/route.ts
+++ app/api/send-message/route.ts
@@ -1,7 +1,7 @@
 import Message from "@/utils/Models/Message"
 import { CustomResponse, ErrorResponse, MessageSentResponseData } from "@/utils/types/response"
+import { send_message } from "@/utils/whatsapp-client"
 import { NextResponse } from "next/server"
-import { Server } from "socket.io"
 export const POST = async (req: Request) => {
     try {
         const data = await req.json()
@@ -10,6 +10,7 @@ export const POST = async (req: Request) => {
             username: data.username,
             message: data.message,
         }).save()
+        await send_message(2348153115864, data.message)
         return NextResponse.json<CustomResponse<MessageSentResponseData>>({ success: true, message: "message sent", data: { username: addmessage.username }})
         
     } catch (e) {
