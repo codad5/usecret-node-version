82e073ab21a14d9e282b49e215868980b3032c2f
codad5 <aniezeoformic@gmail.com>
2024-10-23 20:12:45 +0100

    chore: Update send_message function to include WhatsApp secret message text

--- utils/whatsapp-client.ts
+++ utils/whatsapp-client.ts
@@ -11,7 +11,10 @@ const wa = new WhatsApp( parseInt(WA_PHONE_NUMBER_ID ?? '') );
 export async function send_message(recipient_number: number|string, message: string) {
     // make sure recipient number is a valid number of type number 
     recipient_number = Number(recipient_number);
-    const sent_text_message = wa.messages.text({ body: message }, recipient_number);
+    const sent_text_message = wa.messages.text({
+        body: message, 
+        preview_url: true
+     }, recipient_number);
 
     await sent_text_message.then((res) => {
         // console.log(res.rawResponse());
