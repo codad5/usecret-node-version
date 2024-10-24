913498980a67338a4be3cc0868677621f0c1ce03
codad5 <aniezeoformic@gmail.com>
2024-10-24 12:00:28 +0100

    chore: Update Redis client configuration to log connection details

--- utils/redis.ts
+++ utils/redis.ts
@@ -1,21 +1,22 @@
 import Redis from "ioredis"
 
 const {UPSTASH_REDIS_REST_PASSWORD = null, UPSTASH_REDIS_REST_HOST, UPSTASH_REDIS_REST_PORT, UPSTASH_REDIS_REST_USER = null, UPSTASH_REDIS_REST_DB = 0} = process.env
-const redisClient = new Redis({
+const redisOptions = {
     host: UPSTASH_REDIS_REST_HOST,
     password: UPSTASH_REDIS_REST_PASSWORD || undefined,
     port: parseInt(UPSTASH_REDIS_REST_PORT || "6379"),
     username: UPSTASH_REDIS_REST_USER || undefined,
     db: parseInt(UPSTASH_REDIS_REST_DB || "0")
-})
+}
+const redisClient = new Redis(redisOptions)
 
 
 redisClient.on("connect", () => {
-    console.log("Connected to Redis ⚡️")
+    console.log("Connected to Redis ⚡️with options", redisOptions)
 })
 
 redisClient.on("error", (error) => {
-    console.log("Error connecting to Redis ❌", error)
+    console.log("Error connecting to Redis ❌", error , redisOptions)
 })
 
 export default redisClient;
